1fb3c7ff822158af150306908e020e9c37a3f3b2
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-04 15:30:49 +0100

    loading products

--- src/App.js
+++ src/App.js
@@ -1,35 +1,50 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import customers from './data/customers.json';
 import orders from './data/orders.json';
 
-const colors = ['yellow', 'orange', 'purple', 'blue', 'green'];
-colors.sort();
-console.log(colors);
+// const colors = ['yellow', 'orange', 'purple', 'blue', 'green'];
+// colors.sort();
+// console.log(colors);
 
+const productsUrl = 'https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/json/products.json';
 
 function App() {
-  return (
-    <div className="App">
-		<h1>Northwind Site</h1>
-		  <div>There are {customers.length} customers.</div>
-		  <div>There are {orders.length} orders.</div>
-		  <ul>
-			  {customers.map(customer => {
-				  return (
-					  <li key={customer.customerID}>{customer.companyName}
-						  <ul>
-							  {orders.filter(m => m.customerID === customer.customerID).sort((a,b) => a.orderID - b.orderID).map((order, index, arr) => {
-								  return (
-									  <li key={index}>{order.orderID} - {order.orderDate.substring(0, 10)} ({index + 1} of {arr.length})</li>
-								  )
-							  })}
-						  </ul> 
-					  </li>
-				 )
-			 })}
-		  </ul>
-    </div>
-  );
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		setTimeout(() => {
+			(async () => {
+				const response = await fetch(productsUrl);
+				const data = await response.json();
+				setProducts(data);
+			})();
+		}, 2000);
+	}, []);
+
+	return (
+		<div className="App">
+			<h1>Northwind Site</h1>
+			<div>There are {customers.length} customers.</div>
+			<div>There are {orders.length} orders.</div>
+			<div>There are {products.length} products.</div>
+			<ul>
+				{customers.map(customer => {
+					return (
+						<li key={customer.customerID}>{customer.companyName}
+							<ul>
+								{orders.filter(m => m.customerID === customer.customerID).sort((a, b) => a.orderID - b.orderID).map((order, index, arr) => {
+									return (
+										<li key={index}>{order.orderID} - {order.orderDate.substring(0, 10)} ({index + 1} of {arr.length})</li>
+									)
+								})}
+							</ul>
+						</li>
+					)
+				})}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
