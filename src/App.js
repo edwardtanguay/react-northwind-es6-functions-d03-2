c57964e4a226a58d6b673c8bad94fadc35e22e43
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-04 14:22:25 +0100

    orders are in

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import customers from './data/customers.json';
+import orders from './data/orders.json';
 
 function App() {
   return (
     <div className="App">
 		<h1>Northwind Site</h1>
 		  <div>There are {customers.length} customers.</div>
+		  <div>There are {orders.length} orders.</div>
 		  <ul>
 			  {customers.map(customer => {
 				  return (
